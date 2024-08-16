8bed67e73488dc31b93790a76489dd9ae0864bce
TestSeung <sjlim589@hanmail.net>
2024-08-16 13:43:48 +0900

    refresh,  loading page

--- src/App.svelte
+++ src/App.svelte
@@ -6,14 +6,45 @@
   import Router from "svelte-spa-router";
   import NotFound from "./pages/NotFound.svelte";
   import "./css/style.css";
+  import { user$ } from "./store";
+  import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+
+  const auth = getAuth();
+
+  let isLoading = true;
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
+
   //route
   const routes = {
     "/": Main,
-    "/login": Login,
     "/signup": Signup,
     "/write": Write,
     "*": NotFound, //예외처리
   };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
---
+++ src/pages/Loading.svelte
@@ -0,0 +1,12 @@
+<div>로딩중입니다</div>
+
+<style>
+  div {
+    width: 100vw;
+    height: 100vh;
+    background-color: antiquewhite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
