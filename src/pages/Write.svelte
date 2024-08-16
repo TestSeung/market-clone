e9b1ed6757cb15212af7f830ba31cf36fa8d92d9
TestSeung <sjlim589@hanmail.net>
2024-08-16 10:47:35 +0900

    image upload

--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,27 +1,59 @@
 <script>
   import { getDatabase, ref, push } from "firebase/database";
+  import Footer from "../components/Footer.svelte";
+  import {
+    getStorage,
+    ref as refImage,
+    uploadBytes,
+    getDownloadURL,
+  } from "firebase/storage";
 
   let title;
   let price;
   let description;
   let place;
+  let files;
 
-  function writeUserData(event) {
-    const db = getDatabase();
+  const storage = getStorage();
+  const db = getDatabase();
+  // 'file' comes from the Blob or File API
+  // uploadBytes(storageRef, file).then((snapshot) => {
+  //   console.log('Uploaded a blob or file!');
+  // });
+
+  function writeUserData(imgUrl) {
     push(ref(db, "items/"), {
       title,
       price,
       description,
       place,
+      insertAt: new Date().getTime(),
+      imgUrl,
     });
+    alert("글쓰기가 완료되었습니다."); //서비스 시 사용 지양
+    window.location.hash = "/";
   }
+
+  const uploadFile = async () => {
+    const file = files[0];
+    const name = file.name;
+    const imgRef = refImage(storage, name);
+    const res = await uploadBytes(imgRef, file);
+    const url = await getDownloadURL(imgRef);
+    return url;
+  };
+
+  const handleSubmit = async () => {
+    const url = await uploadFile();
+    writeUserData(url);
+  };
 </script>
 
-<form id="write-form" on:submit|preventDefault={writeUserData}>
-  <!-- <div>
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
+  <div>
     <label for="">이미지</label>
-    <input type="file" id="image" name="image" />
-  </div> -->
+    <input type="file" bind:files id="image" name="image" />
+  </div>
   <div>
     <label for="">제목</label>
     <input type="text" id="title" name="title" bind:value={title} />
@@ -44,6 +76,19 @@
     <input type="text" id="place" name="place" bind:value={place} />
   </div>
   <div>
-    <button type="submit">글쓰기 완료!</button>
+    <button class="write-button" type="submit">글쓰기 완료!</button>
   </div>
 </form>
+
+<Footer location="Write" />
+
+<style>
+  .write-button {
+    background-color: tomato;
+    margin: 10px;
+    border-radius: 10px;
+    padding: 5px 12px 5px 12px;
+    color: white;
+    cursor: pointer;
+  }
+</style>
